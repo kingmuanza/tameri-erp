1c030ab2a1a5bdee818b6040dac79f505bd59887
unknown <muanza.kangudie@gmail.com>
2022-12-05 23:19:13 +0100

    stock

--- tameri-front/src/app/pages/product/product-list/product-list.component.ts
+++ tameri-front/src/app/pages/product/product-list/product-list.component.ts
@@ -6,6 +6,8 @@ import { DatatablesOptions } from 'src/app/_data/datatable.option';
 import { Company } from 'src/app/_models/company.model';
 import { Employee } from 'src/app/_models/employee.model';
 import { Product } from 'src/app/_models/product.model';
+import { Productitem } from 'src/app/_models/productitem.model';
+import { Saleline } from 'src/app/_models/saleline.model';
 import { AuthenticationService } from 'src/app/_services/authentication.service';
 import { CrudService } from 'src/app/_services/crud.service';
 
@@ -23,12 +25,16 @@ export class ProductListComponent implements OnInit {
   dtInstance!: Promise<DataTables.Api>;
 
   products = new Array<Product>();
+  salelines = new Array<Saleline>();
+  productitems = new Array<Productitem>();
   company = new Company();
 
   constructor(
     private router: Router,
     private productService: CrudService<Product>,
     private authService: AuthenticationService,
+    private productitemService: CrudService<Productitem>,
+    private salelineService: CrudService<Saleline>,
   ) {
     this.company = this.authService.user.company;
   }
@@ -48,7 +54,7 @@ export class ProductListComponent implements OnInit {
     return dtOptions;
   }
 
-  edit(product?:Product) {
+  edit(product?: Product) {
     if (product) {
       this.router.navigate(['product', 'view', product.id]);
     } else {
@@ -57,19 +63,68 @@ export class ProductListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.dtOptions = this.initNouveau();
-    this.productService.getAll('product').then((data) => {
-      this.products = data.filter((d) => {
-        return d.company && d.company.id === this.company.id;
+
+    this.salelineService.getAll('saleline').then((salelines) => {
+      this.salelines = salelines.filter((d) => {
+        return true;
       });
-      this.dtTrigger.next('');
-    }).catch((e)=> {
-      this.dtTrigger.next('');
+
+      this.productitemService.getAll('productitem').then((data) => {
+        this.productitems = data.filter((d) => {
+          const isCompany = d.company && d.company.id === this.company.id;
+          return isCompany;
+        });
+      });
+
+      this.dtOptions = this.initNouveau();
+      this.productService.getAll('product').then((data) => {
+        this.products = data.filter((d) => {
+          return d.company && d.company.id === this.company.id;
+        });
+        this.dtTrigger.next('');
+      }).catch((e) => {
+        this.dtTrigger.next('');
+      });
     });
+  }
+
+  getSalelines(product: Product) {
+    const salelines = this.salelines.filter((d) => {
+      return d.productpack.product.id === product.id;
+    });
+    const totalSales = this.calculTotalSales(salelines);
+    return totalSales;
+    // this.getProductItems(product);
+  }
+
+  getProductItems(product: Product) {
+    const productitems = this.productitems.filter((d) => {
+      const isProduit = d.product && d.product.id === product.id;
+      return isProduit;
+    });
+    const totalItems = this.calculTotalItems(productitems);
+    return totalItems;
+
+  }
+
+  calculTotalSales(salelines: Array<Saleline>) {
+    let total = 0;
+    salelines.forEach((s) => {
+      total += s.quantity;
+    });
+    return total;
+  }
+
+  calculTotalItems(productitems = new Array<Productitem>()) {
+    let total = 0;
+    productitems.forEach((s) => {
+      total += s.quantity;
+    });
+    return total;
   }
 
   ngOnDestroy(): void {
     this.dtTrigger.unsubscribe();
   }
-  
+
 }
